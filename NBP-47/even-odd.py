22de22ce4e999fee6de5dde55caa8568b18f1b31
surajpaudel6741 <surajpaudel696741@gmail.com>
2025-01-15 17:54:25 +0545

    Programs updated

--- NBP-47/even-odd.py
+++ NBP-47/even-odd.py
@@ -1,7 +1,15 @@
-a = int (input("Enter any number"))
-while a!=1:
-    if a % 2 ==0:
-        a = a // 2 
-    else:
-        a = a * 3 +1
+a = 516
+sequence = []
+
+while True:
+    a = a // 2 if a % 2 == 0 else a * 3 + 1
+    sequence.append(a)
+    
+    print(sequence)
+
+    if len(sequence) == 4:
+        if sequence[1] == 4 and sequence[2] == 2:
+            break
+        sequence = []
+
 print(a)
--- NBP-47/least-no-of-loops.py
+++ NBP-47/least-no-of-loops.py
@@ -1,20 +1,21 @@
-import math
+# import math
     
-a =int( input("Enter a number"))
-b = 0
-count = 0
-if a==1 or a == 0:
-    b = 1
-else:
-    for i in range(2, int (math.sqrt(a))):
-        count = count + 1
-        if a % i ==0:
-            b = 1
-if b == 0:
-    print(a, "is the prime number")
-else:
-    print(a, "is not a prime number")
-print (count)
+# a =int( input("Enter a number"))
+# b = 0
+# count = 0
+# if a==1 or a == 0:
+#     b = 1
+# else:
+#     for i in range(2, int (math.sqrt(a))):
+#         count = count + 1
+#         if a % i ==0:
+#             b = 1
+# if b == 0:
+#     print(a, "is the prime number")
+# else:
+#     print(a, "is not a prime number")
+# print (count)
+
 
 # a =int( input("Enter a number"))
 # b = 0
@@ -47,3 +48,18 @@ print (count)
 # else:
 #     print(a, "is not a prime number")
 # print (count)
+
+n = 200
+prime = [1] * (n + 1)
+
+prime[0] = 0
+prime[1] = 0
+
+for j in range(2, int(n ** 0.5) + 1):
+    if prime[j] == 1:
+        for i in range(2, (n // j) + 1):
+            prime[i * j] = 0
+
+for i in range(n + 1):
+    if prime[i] == 1:
+        print(i)
